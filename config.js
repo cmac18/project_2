fb19de0000cac6a065ea6d51110b8aa8176fc779
Cmac <364mc@att.net>
2020-03-20 15:39:34 -0500

    Uploded Files

---
+++ config.js
@@ -0,0 +1,2 @@
+// API Key
+const API_KEY = "pk.eyJ1IjoiY21hYzE4IiwiYSI6ImNrN3JwOTFvdzBneWIzZ2s1aWpsNGwwdXUifQ._SNP87O7Ni29bG8kc2iqmw";
---
+++ logic.js
@@ -0,0 +1,16 @@
+// Create a map object
+var myMap = L.map("map", {
+    center: [15.5994, -28.6731],
+    zoom: 3
+  });
+  // Adding tile layer
+  L.tileLayer(
+    "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox.satellite",
+      accessToken: API_KEY
+    }
+  ).addTo(myMap);
