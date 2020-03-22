777479d2937aeb1e9b7d8037d6065103c6c2981b
Cmac <364mc@att.net>
2020-03-22 11:03:39 -0500

    edited the logic file

--- logic.js
+++ logic.js
@@ -1,16 +1,38 @@
-// Create a map object
+  // Create a map object
 var myMap = L.map("map", {
-    center: [15.5994, -28.6731],
-    zoom: 3
-  });
-  // Adding tile layer
-  L.tileLayer(
-    "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
-    {
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: "mapbox.satellite",
-      accessToken: API_KEY
-    }
-  ).addTo(myMap);
+  center: [15.5994, -28.6731],
+  zoom: 3
+});
+// Adding tile layer
+L.tileLayer(
+  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  }
+).addTo(myMap);
+
+
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://corona.lmao.ninja/jhucsse";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+// Once we get a response, send the data.features object to the createFeatures function
+createFeatures(data.features);
+});
+
+const url = "https://corona.lmao.ninja/jhucsse";
+
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+console.log(data);
+});
+
+// Promise Pending
+const dataPromise = d3.json(url);
+console.log("Data Promise: ", dataPromise);
+
